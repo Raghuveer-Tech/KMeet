3fe7d5986f8b54b13365ea6545bb64214b7cde9a
Raghuveer-Tech <raghuveerkumawat.work@gmail.com>
2025-11-12 22:49:50 +0530

    add some

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -26,7 +26,7 @@ function HomeComponent() {
 
                 <div style={{ display: "flex", alignItems: "center" }}>
 
-                    <h2>Apna Video Call</h2>
+                    <h2>Welcome to K-Meet</h2>
                 </div>
 
                 <div style={{ display: "flex", alignItems: "center" }}>
